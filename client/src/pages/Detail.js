1b4246309ce9888d65227ceab2dc1d405db5b8f4
Valerie <valerie.tram.anh.n@gmail.com>
2022-01-30 19:56:35 -0500

    added redux state and actions in detail.js

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,7 +3,7 @@ import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
+// import { useStoreContext } from '../utils/GlobalState';
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -14,8 +14,13 @@ import { QUERY_PRODUCTS } from '../utils/queries';
 import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
 
+import { useSelector, useDispatch } from "react-redux";
+
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  // const [state, dispatch] = useStoreContext();
+  const state = useSelector(state => state);
+  const dispatch = useDispatch(); 
+
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
